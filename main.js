de0c036d8ce37e8254f4ec500f04cccf590d32f1
wrakc <carlosveiga1618@gmail.com>
2020-11-23 19:12:06 -0300

    add navbar

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";({init(){!function(){const t=document.getElementById("content"),e=document.createElement("nav"),n=document.createElement("h1");n.textContent="This is a Restaurant Page",t.appendChild(e),e.appendChild(n)}()}}).init()})();
+(()=>{"use strict";({init(){!function(){const e=document.getElementById("content"),t=document.createElement("nav"),n=document.createElement("h1");n.textContent="Restaurant",e.appendChild(t),t.appendChild(n),t.className="grid grid-cols-1",n.className="place-self-center p-8 font-bold"}()}}).init()})();
