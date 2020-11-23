01ddaebd1102be435277d013604fe536c89f8678
wrakc <carlosveiga1618@gmail.com>
2020-11-23 19:12:06 -0300

    add navbar

--- src/JavaScript/navbar.js
+++ src/JavaScript/navbar.js
@@ -2,7 +2,9 @@ export function navbar() {
   const content = document.getElementById('content');
   const navContainer = document.createElement('nav');
   const title = document.createElement('h1');
-  title.textContent = "This is a Restaurant Page";
+  title.textContent = "Restaurant";
   content.appendChild(navContainer);
   navContainer.appendChild(title);
+  navContainer.className = "grid grid-cols-1";
+  title.className = "place-self-center p-8 font-bold";
 }
