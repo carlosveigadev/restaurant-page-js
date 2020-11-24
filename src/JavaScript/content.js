a6571ad2490cd6bce26da2228ee50b4680ee327a
wrakc <carlosveiga1618@gmail.com>
2020-11-24 12:46:17 -0300

    add main content atributes, properties, and skeleton

--- src/JavaScript/content.js
+++ src/JavaScript/content.js
@@ -1,8 +1,17 @@
 // import tabs
-export function content() {
+export function mainContent() {
   const content = document.getElementById('content');
   const mainContainer = document.createElement('div');
-  const image = document.createElement('img');
+  const restaurantImage = document.createElement('img');
+  const tabs = document.createElement('div');
+  tabs.setAttribute('id', 'tabs');
+  restaurantImage.setAttribute('src', '/src/assets/imgs/restaurant.jpg');
+  restaurantImage.setAttribute('alt', 'Restaurant Image');
+  content.appendChild(mainContainer);
+  mainContainer.appendChild(restaurantImage);
+  mainContainer.appendChild(tabs);
+  mainContainer.className = 'grid grid-cols-2';
+  restaurantImage.className = 'm-10 rounded'
   // tabs
   
   // title.textContent = "Restaurant";
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { content } from './JavaScript/content.js';
+import { mainContent } from './JavaScript/content.js';
 import { navbar } from './JavaScript/navbar.js';
 
 const restaurant = {
   init() {
     navbar();
-    content();
+    mainContent();
   },
 }
 
