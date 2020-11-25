74351e69a83da2914b6493c21814fd89d0b28958
wrakc <carlosveiga1618@gmail.com>
2020-11-25 13:24:06 -0300

    add dessert module

---
+++ src/JavaScript/dessert.js
@@ -0,0 +1,19 @@
+export function dessert() {
+  const tabsDiv = document.getElementById('tabsDiv');
+  const dessertDiv = document.createElement('div');
+  const dessertImg = document.createElement('img');
+  const contentDessert = document.createElement('div');
+  const dessertParagraphs = document.createElement('p');
+  const appetizers = ['Cheesecake with Cranberry Glaze and Sugared Cranberries', 'Caramel Bars', 'Cranberry-Orange Bundt® Cake', 'Almond Milk Chocolate Pudding', "Russian Honey Cake", 'Pumpkin Spice Latte Coffee Cake'];
+  appetizers.forEach(element => {
+    dessertParagraphs.innerHTML += "-" + " " + element + ";<br>";
+  });
+  tabsDiv.appendChild(dessertDiv);
+  dessertDiv.appendChild(dessertImg);
+  dessertDiv.appendChild(contentDessert);
+  contentDessert.appendChild(dessertParagraphs);
+  dessertDiv.className = 'm-24';
+  dessertParagraphs.className = 'font-bold text-lg text-yellow-900';
+  dessertImg.setAttribute('src', '/src/assets/imgs/dessert.png');
+  dessertImg.className = 'w-full rounded';
+}
